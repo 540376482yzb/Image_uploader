2d4e9e6d9ea62c9fbec9ad9f4548d188ba531563
zhou <540376482yzb@gmail.com>
2019-07-01 10:11:02 -0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import ImageLoader from "./ImageLoader";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    showLoader: false
+  };
+  _crop() {
+    // image in dataUrl
+    console.log(this.refs.cropper.getCroppedCanvas().toDataURL());
+  }
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <button className="btn btn-sm btn-primary px-4" onClick={() => this.setState({ showLoader: true })}>
+          点击
+        </button>
+        {this.state.showLoader && <ImageLoader onClose={() => this.setState({ showLoader: false })} />}
+      </div>
+    );
+  }
 }
 
 export default App;
